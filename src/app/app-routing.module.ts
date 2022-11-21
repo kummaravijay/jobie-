df724073729207c81049c38b7b40aff15d895ad7
swathi <dasanagaswathi@gmail.com>
2022-11-21 16:47:27 +0530

    send

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { MainComponent } from './main/main.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
+  
+      {path:'main',component:MainComponent},
+    
   {path:'',redirectTo:'register',pathMatch:'full'},
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,25 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { RegisterComponent } from './register/register.component';
 import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
+import {MatIconModule} from'@angular/material/icon';
+import{MatButtonModule} from'@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     RegisterComponent,
-    FooterComponent
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
