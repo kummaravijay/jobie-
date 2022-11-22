5f115b60aa7677fda270a5ccbdaf211caaec3c24
madhukirangaddam <gaddammadhukiranreddy@gmail.com>
2022-11-22 15:36:38 +0530

    update my branch

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,35 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CandidateprofileComponent } from './candidateprofile/candidateprofile.component';
 import { HeaderComponent } from './header/header.component';
+import { MyjobComponent } from './myjob/myjob.component';
+import { MyprofileComponent } from './myprofile/myprofile.component';
 import { RegisterComponent } from './register/register.component';
+import { SavedjobsComponent } from './savedjobs/savedjobs.component';
 import { SigninComponent } from './signin/signin.component';
 
+
 const routes: Routes = [
-  {path:'register',component:RegisterComponent},
-  {path:'',redirectTo:'register',pathMatch:'full'},
-  {path:'signin',component:SigninComponent}
+  { path: 'register', component: RegisterComponent },
+  { path: '', redirectTo: 'register', pathMatch: 'full' },
+  {
+    path:'candidateprofile', component: CandidateprofileComponent,
+    children: [
+      // {
+      //   path:'',component:MyprofileComponent
+      // },
+      {
+        path: 'myprofile',component: MyprofileComponent
+      },
+      {
+        path: 'myjob',component:MyjobComponent
+      },
+      {
+        path:'savedjobs',component:SavedjobsComponent
+      }]
+
+  },
+  { path: 'signin',component:SigninComponent }
 ];
 
 @NgModule({
---
+++ src/app/savedjobs/savedjobs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-savedjobs',
+  templateUrl: './savedjobs.component.html',
+  styleUrls: ['./savedjobs.component.scss']
+})
+export class SavedjobsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
