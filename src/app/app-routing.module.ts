9f35502356a9c0be0ea719ad76afebf36de5c067
kuruba sneha latha <kuruba.snehalatha@gmail.com>
2022-11-23 13:31:43 +0530

    x

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { JobiboxComponent } from './jobibox/jobibox.component';
 import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {path:'register',component:RegisterComponent},
   {path:'',redirectTo:'register',pathMatch:'full'},
+  {path:'jobibox',component:JobiboxComponent}
 ];
 
 @NgModule({
