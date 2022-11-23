46fc5169904eb8b5d358512fbda78740f1ff5c36
Sarithayerragudi <sariyerragudi@gmail.com>
2022-11-23 11:04:18 +0530

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CandidateprofileComponent } from './candidateprofile/candidateprofile.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
 import { MyjobComponent } from './myjob/myjob.component';
 import { MyprofileComponent } from './myprofile/myprofile.component';
 import { RegisterComponent } from './register/register.component';
@@ -26,7 +27,11 @@ const routes: Routes = [
       },
       {
         path:'savedjobs',component:SavedjobsComponent
-      }]
+      },
+      {
+        path:'home',component:HomeComponent
+      }
+    ]
 
   },
   { path: 'signin',component:SigninComponent }
